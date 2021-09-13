4138dc8b7ed3f9e3a6233a91731fe01f15ea388f
milyukov <9ellameon9@mail.ru>
2021-09-13 10:32:52 +0300

    massiv 1 done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
-const name = prompt('введите имя')
-const age = prompt('возраст')
 
-let personDesc = function (name, age) {
-    if (1 <= age && age <= 17) {
-        console.log(`${name} имеет возраст ${age} и он ребенок`)
-    }
-    else if (18 <= age && age <= 30)
-        console.log(`${name} имеет возраст ${age} и он молодой`)
-    else if (31 <= age && age <= 55)
-        console.log(`${name} имеет возраст ${age} и он взрослый`)
-    else if (56 <= age && age <= 100)
-        console.log(`${name} имеет возраст ${age} и он старый`)
-}
 
-personDesc(name, age)
+
+let Integers = [4, 2, 20, 8, 10]
+let result = false
+for (let i in Integers)
+result = Integers[i] % 2 === 0;
+
+alert(result)
+
