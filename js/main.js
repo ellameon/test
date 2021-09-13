f90c30aba48caac0a32af86b01638ebc98a91d01
milyukov <9ellameon9@mail.ru>
2021-09-13 11:32:33 +0300

    massiv 4 done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-let numbers = [1, 2, 5, 12, 15, 21, 25, 30, 41]
 
-let resultArray = numbers.filter(elem => elem.valueOf() % 5 === 0 )
 
-console.log(resultArray)
+
+function averageSum (arr) {
+ let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum / arr.length
+}
+
+console.log(averageSum([6, 2, 4, 12, 8, 20, 26, 30, 40, 80]))
