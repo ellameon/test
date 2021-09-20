53d45967bccc0c92b63801fde9ab15eede1ac841
milyukov <9ellameon9@mail.ru>
2021-09-20 08:10:54 +0300

    clear master

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-//Выводим кнопку с текстом "Поменять" и два инпута,
-// при клике на кнопку инпуты меняются своим введеным текстом
-
-function changeInputs() {
-    let input1 = document.getElementById('input1')
-    let input2 = document.getElementById('input2')
-
-    const input1Value = input1.value
-    input1.value = input2.value
-    input2.value = input1Value
-    }
 
 
 
