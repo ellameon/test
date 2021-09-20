7ee0187f8db120f1c505513e9ccc121c9fd309df
milyukov <9ellameon9@mail.ru>
2021-09-20 07:27:37 +0300

    Dom1 done

--- js/main.js
+++ js/main.js
@@ -1,22 +1,2 @@
-let ladder = {
-    step: 0,
-    up() {
-        this.step++;
-        return this;
-    },
-    down() {
-        this.step--;
-        return this;
-    },
-    showStep() {
-        console.log( this.step );
-        return this;
-    }
-}
+//Вывести кнопку с текстом "Привет", при нажатии на неё выводим alert c текстом "Привет Мир!"
 
-ladder.showStep()
-ladder.up()
-ladder.up()
-ladder.showStep()
-ladder.down()
-ladder.showStep()
