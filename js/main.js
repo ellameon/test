f3c6b77e97acbc715604c3c74ce93da890fd52fd
milyukov <9ellameon9@mail.ru>
2021-09-20 11:10:55 +0300

    reg1 done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+//проверка номера телефона с регулярными выражениями
 
+function telephoneCheck(str) {
+    const reg = new RegExp(/^(\+*)(\d*)([(]\d{1,3}[)]*)*(\s?\d+|\+\d{2,3}\s\d+|\d+)[\s|-]?\d+([\s|-]?\d+){1,2}(\s)*$/gm)
+    return reg.test(str)
+}
 
-
-
-
+alert(telephoneCheck("89211242120"))
